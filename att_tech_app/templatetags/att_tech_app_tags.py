bf2ba61ab6afef38e974b2f86fafdfab7a744d65
Stas Kotseruba <odin793@gmail.com>
2013-04-28 22:37:19 +0400

    slideshow template changes. quote style changes. 6 news in news block

--- att_tech_app/templatetags/att_tech_app_tags.py
+++ att_tech_app/templatetags/att_tech_app_tags.py
@@ -6,5 +6,5 @@ register = template.Library()
 
 @register.inclusion_tag('news_block_template.html')
 def news_block():
-    last_news = New.objects.all().order_by('-date_added')[:7]
+    last_news = New.objects.all().order_by('-date_added')[:6]
     return {'last_news': last_news}
